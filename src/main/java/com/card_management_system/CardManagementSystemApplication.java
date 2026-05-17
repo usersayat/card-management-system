2b6a011b36770a34a131aec98fef7d34fa38472c
Sayat <sayatomsk@bk.ru>
2026-05-17 15:33:53 +0600

    initial commit

---
+++ src/main/java/com/card_management_system/CardManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.card_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardManagementSystemApplication.class, args);
+	}
+
+}
